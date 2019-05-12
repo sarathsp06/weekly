434b6f3132f65750ee262041e43bb881390507a9
sarathsp06 <sarath.sp06@gmail.com>
2019-05-12 23:28:17 +0200

    irctc: added schedule plot for some trains with distance in y axis

---
+++ irctc/libs.py
@@ -0,0 +1,7 @@
+import numpy as np
+import scipy as sp
+from matplotlib import pyplot as plt
+import matplotlib.dates as md
+import pandas as pd
+from pandas.plotting import register_matplotlib_converters
+register_matplotlib_converters()
